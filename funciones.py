28c6b30d44a1fc0aa9ec9d3cf53d2bc548152362
Marianito412 <jmarianosoto@gmail.com>
2023-04-10 15:23:54 -0600

    se agregó decodificación de torta chilena y queque

--- funciones.py
+++ funciones.py
@@ -3,5 +3,27 @@
 #Ultima version: 30/3/2023 1:30pm
 #Version: 3.10.6
 
+import re
+
+def decodificarQueque(pCodigo):
+    if re.match("\w{5}", pCodigo):
+        codigoTamannos = ["PQ", "Pequeño", "GR", "Grande"]
+        tamanno = codigoTamannos[codigoTamannos.index(pCodigo[2:4])+1]
+
+        codigoSabor = ["FR", "Fresa", "VN", "Vainilla", "CM","Caramelo", "CE", "Chocolate"]
+        sabor = codigoSabor[codigoSabor.index(pCodigo[4:6])+1]
+        return f"Usted solicita un queque de sabor de {sabor}, de tamaño: {tamanno}"
+    return "Código inválido"
+
+def decodificarTorta(pCodigo):
+    if re.match("\w{5}", pCodigo):
+        if pCodigo == "TCAGR":
+            return "Usted solicita una torta chilena, de tamaño: grande"
+    return "Código inválido"
+
 def decodificarProducto(pCodigo):
-    return "Mjm, definitivamente es comida"
+    #return "Mjm, definitivamente es comida"
+    if "QE" == pCodigo[:2]:
+        return decodificarQueque(pCodigo)
+    elif "TCA" == pCodigo[:3]:
+        return decodificarTorta(pCodigo)
