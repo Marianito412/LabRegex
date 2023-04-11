8623733a3d322259606f82b52eacfe038263189d
niparra21 <niparra@estudiantec.cr>
2023-04-10 20:51:12 -0600

    reposteria

--- funciones.py
+++ funciones.py
@@ -5,53 +5,26 @@
 
 import re
 
-def decodificarProducto(pCodigo):
-    resultado=""
-    if re.match('red\d',pCodigo):
-        resultado="Usted solicita una reposteria de sabor dulce, correspondiente a articulo: tipo."
-        if re.match('1',pCodigo[3]):
-            resultado=resultado.replace("tipo","nidito")
-        elif re.match('4',pCodigo[3]):
-            resultado=resultado.replace("tipo","biscuit")
-        elif re.match('5',pCodigo[3]):
-            resultado=resultado.replace("tipo","crocante")
-        elif re.match('3',pCodigo[3]):
-            resultado=resultado.replace("tipo","orejita")
-        else:
-            return "Debe digitar un codigo valido"
-    elif re.match('res\d',pCodigo):
-        resultado="Usted solicita una reposteria de sabor salada, correspondiente a articulo: tipo."
-        if re.match('2',pCodigo[3]):
-            resultado=resultado.replace("tipo","palito de queso")
-        elif re.match('6',pCodigo[3]):
-            resultado=resultado.replace("tipo","enchilada")
-        else:
-            return "Debe digitar un codigo valido"
-    elif re.match('res\w{4}',pCodigo) :
-        resultado="Usted solicita una reposteria de sabor salada, correspondiente a articulo: tipo de sabor, de tamaño medida."
-        if re.match('7',pCodigo[3]):
-            resultado=resultado.replace("tipo","empanada")
-            if re.match('1',pCodigo[4]):
-                resultado=resultado.replace("sabor","pollo")
-            elif re.match('2',pCodigo[4]):
-                resultado=resultado.replace("sabor","carne")
-            else:
-                return "Solo hay dos sabores"
-            if re.match('pq',pCodigo[5:]):
-                resultado=resultado.replace("medida","pequeña")
-            elif re.match('md',pCodigo[5:]):
-                resultado=resultado.replace("medida","mediana")
-            elif re.match('gd',pCodigo[5:]):
-                resultado=resultado.replace("medida","grande")
-            else:
-                return "Solo hay tres tamaños"
+def decodificarReposteria(pCodigo):
+    pCodigo=pCodigo.upper()
+    if re.match('[1245]',pCodigo[3]):
+        articulo="un"
+    elif re.match('[367]',pCodigo[3]):
+        articulo="una"
+    if re.match('^(RED[1345])$',pCodigo):
+        tiposDulces=["1","nidito","4","biscuit","5","crocante","3","orejita"]
+        tipo=tiposDulces[tiposDulces.index(pCodigo[3])+1]
+        return f"Usted solicita una reposteria de sabor dulce, correspondiente a {articulo}: {tipo}."
+    if re.match('^(RES[26])$',pCodigo):
+        tiposSalados=["2","palito de queso","6","enchilada"]
+        tipo=tiposSalados[tiposSalados.index(pCodigo[3])+1]
+        return f"Usted solicita una reposteria de sabor salado, correspondiente a {articulo}: {tipo}."
     else:
-        return "Digite un codigo valido"
-    return resultado
-                
-            
-    """if re.match('[1245]',pCodigo[3]):
-        resultado=resultado.replace("articulo","un")
-        if re.match('[367]',pCodigo[3]):
-            resultado=resultado.replace("articulo","una")
-        return resultado"""
+        if re.match('^(RES[12]\w{2})$',pCodigo):
+            sabores=["1","pollo","2","carne"]
+            sabor=sabores[sabores.index(pCodigo[4])+1]
+            tamannos=["PQ","pequeña","MD","mediana","GR","grande"]
+            medida=tamannos[tamannos.index(pCodigo[5:])+1]
+            return f"Usted solicita una reposteria de sabor salada, correspondiente a {articulo}: empanada de {sabor}, de tamaño {medida}."
+        else:
+            return "Digite un codigo valido"
